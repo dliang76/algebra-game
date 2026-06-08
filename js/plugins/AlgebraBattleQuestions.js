56d7ec98fb028f99cad91c02caf184418d265218
Dennis Liang <dennis@Dennis-iMac.fios-router.home>
2026-06-08 19:16:20 -0400

    added time limit to math problem solving

--- js/plugins/AlgebraBattleQuestions.js
+++ js/plugins/AlgebraBattleQuestions.js
@@ -21,6 +21,25 @@
     "use strict";
 
     const QUESTION_FILE = "algebra_questions.json";
+    const QUESTION_TIME_LIMIT_SECONDS = 30;
+    const SUPERSCRIPTS = {
+        "0": "⁰",
+        "1": "¹",
+        "2": "²",
+        "3": "³",
+        "4": "⁴",
+        "5": "⁵",
+        "6": "⁶",
+        "7": "⁷",
+        "8": "⁸",
+        "9": "⁹",
+        "+": "⁺",
+        "-": "⁻",
+        "(": "⁽",
+        ")": "⁾",
+        "n": "ⁿ",
+        "i": "ⁱ"
+    };
 
     const AlgebraBattleQuestions = {
         _loadStarted: false,
@@ -89,6 +108,24 @@
 
         randomQuestion() {
             return this._questions[Math.randomInt(this._questions.length)];
+        },
+
+        formatMathText(value) {
+            return String(value)
+                .replace(/\^(-?\d+)/g, (_, exponent) => {
+                    return exponent
+                        .split("")
+                        .map(character => SUPERSCRIPTS[character] || character)
+                        .join("");
+                })
+                .replace(/\*/g, " × ")
+                .replace(/\s*=\s*/g, " = ")
+                .replace(/\s*\+\s*/g, " + ")
+                .replace(/\s*-\s*/g, " - ")
+                .replace(/^\s-\s/, "-")
+                .replace(/\(\s-\s/g, "(-")
+                .replace(/\s+/g, " ")
+                .trim();
         },
 
         ensureSkipSkill() {
@@ -174,7 +211,8 @@
         this.changeTextColor(ColorManager.systemColor());
         this.drawText("Answer correctly to attack:", 0, 0, this.innerWidth);
         this.resetTextColor();
-        this.drawWrappedText(this._question.question, 0, this.lineHeight() + 8, this.innerWidth);
+        const question = AlgebraBattleQuestions.formatMathText(this._question.question);
+        this.drawWrappedText(question, 0, this.lineHeight() + 8, this.innerWidth);
     };
 
     Window_AlgebraQuestionPrompt.prototype.drawWrappedText = function(text, x, y, maxWidth) {
@@ -206,6 +244,7 @@
     Window_AlgebraChoices.prototype.initialize = function(rect) {
         this._question = null;
         Window_Command.prototype.initialize.call(this, rect);
+        this.deactivate();
         this.hide();
         this.close();
     };
@@ -221,12 +260,50 @@
             return;
         }
         this._question.choices.forEach((choice, index) => {
-            this.addCommand(choice, `answer${index}`, true, index);
+            const formattedChoice = AlgebraBattleQuestions.formatMathText(choice);
+            this.addCommand(formattedChoice, `answer${index}`, true, index);
         });
     };
 
     Window_AlgebraChoices.prototype.itemHeight = function() {
         return Math.max(44, Window_Command.prototype.itemHeight.call(this));
+    };
+
+    function Window_AlgebraTimer() {
+        this.initialize(...arguments);
+    }
+
+    Window_AlgebraTimer.prototype = Object.create(Window_Base.prototype);
+    Window_AlgebraTimer.prototype.constructor = Window_AlgebraTimer;
+
+    Window_AlgebraTimer.prototype.initialize = function(rect) {
+        Window_Base.prototype.initialize.call(this, rect);
+        this._remainingSeconds = null;
+        this.hide();
+        this.close();
+    };
+
+    Window_AlgebraTimer.prototype.setRemainingSeconds = function(seconds) {
+        const roundedSeconds = Math.max(0, Math.ceil(seconds));
+        if (this._remainingSeconds !== roundedSeconds) {
+            this._remainingSeconds = roundedSeconds;
+            this.refresh();
+        }
+    };
+
+    Window_AlgebraTimer.prototype.refresh = function() {
+        this.contents.clear();
+        const rate = this._remainingSeconds / QUESTION_TIME_LIMIT_SECONDS;
+        const labelWidth = 120;
+        const barX = labelWidth + 16;
+        const barY = Math.floor(this.innerHeight / 2) - 7;
+        const barWidth = this.innerWidth - barX;
+        const barColor = rate <= 0.25 ? ColorManager.crisisColor() : ColorManager.normalColor();
+        this.changeTextColor(rate <= 0.25 ? ColorManager.crisisColor() : ColorManager.systemColor());
+        this.drawText(`Time: ${this._remainingSeconds}s`, 0, 0, labelWidth, "left");
+        this.contents.fillRect(barX, barY, barWidth, 14, ColorManager.gaugeBackColor());
+        this.contents.fillRect(barX, barY, Math.floor(barWidth * rate), 14, barColor);
+        this.resetTextColor();
     };
 
     const _Scene_Battle_createAllWindows = Scene_Battle.prototype.createAllWindows;
@@ -239,20 +316,52 @@
         const margin = 48;
         const width = Math.min(Graphics.boxWidth - margin * 2, 1040);
         const x = Math.floor((Graphics.boxWidth - width) / 2);
+        const timerHeight = this.calcWindowHeight(1, false);
         const promptHeight = this.calcWindowHeight(4, false);
         const choiceHeight = this.calcWindowHeight(5, true);
-        const y = Math.max(16, Math.floor((Graphics.boxHeight - promptHeight - choiceHeight - 16) / 2));
+        const y = Math.max(16, Math.floor((Graphics.boxHeight - timerHeight - promptHeight - choiceHeight - 32) / 2));
 
+        this._algebraTimerWindow = new Window_AlgebraTimer(
+            new Rectangle(x, y, width, timerHeight)
+        );
         this._algebraPromptWindow = new Window_AlgebraQuestionPrompt(
-            new Rectangle(x, y, width, promptHeight)
+            new Rectangle(x, y + timerHeight + 8, width, promptHeight)
         );
         this._algebraChoiceWindow = new Window_AlgebraChoices(
-            new Rectangle(x, y + promptHeight + 16, width, choiceHeight)
+            new Rectangle(x, y + timerHeight + promptHeight + 24, width, choiceHeight)
         );
         this._algebraChoiceWindow.setHandler("ok", this.onAlgebraAnswerOk.bind(this));
-        this._algebraChoiceWindow.setHandler("cancel", this.onAlgebraAnswerCancel.bind(this));
+        this.addWindow(this._algebraTimerWindow);
         this.addWindow(this._algebraPromptWindow);
         this.addWindow(this._algebraChoiceWindow);
+    };
+
+    const _Scene_Battle_update = Scene_Battle.prototype.update;
+    Scene_Battle.prototype.update = function() {
+        _Scene_Battle_update.call(this);
+        this.updateAlgebraQuestionTimer();
+    };
+
+    const _Scene_Battle_isAnyInputWindowActive = Scene_Battle.prototype.isAnyInputWindowActive;
+    Scene_Battle.prototype.isAnyInputWindowActive = function() {
+        return this.isAlgebraQuestionActive() || _Scene_Battle_isAnyInputWindowActive.call(this);
+    };
+
+    const _Scene_Battle_updateCancelButton = Scene_Battle.prototype.updateCancelButton;
+    Scene_Battle.prototype.updateCancelButton = function() {
+        _Scene_Battle_updateCancelButton.call(this);
+        if (this._cancelButton && this.isAlgebraQuestionActive()) {
+            this._cancelButton.visible = false;
+        }
+    };
+
+    Scene_Battle.prototype.isAlgebraQuestionActive = function() {
+        return (
+            !!this._algebraChoiceWindow &&
+            this._algebraChoiceWindow.active &&
+            this._algebraChoiceWindow.visible &&
+            this._algebraChoiceWindow.isOpen()
+        );
     };
 
     const _Scene_Battle_commandAttack = Scene_Battle.prototype.commandAttack;
@@ -266,11 +375,18 @@
 
     Scene_Battle.prototype.startAlgebraQuestion = function() {
         this._algebraQuestion = AlgebraBattleQuestions.randomQuestion();
+        this._algebraQuestionTimerFrames = QUESTION_TIME_LIMIT_SECONDS * 60;
+        this._algebraQuestionTimedOut = false;
+        this._partyCommandWindow.deactivate();
+        this._partyCommandWindow.close();
         this._actorCommandWindow.deactivate();
         this._actorCommandWindow.hide();
         this._statusWindow.hide();
         this._algebraPromptWindow.setQuestion(this._algebraQuestion);
         this._algebraChoiceWindow.setQuestion(this._algebraQuestion);
+        this._algebraTimerWindow.setRemainingSeconds(QUESTION_TIME_LIMIT_SECONDS);
+        this._algebraTimerWindow.show();
+        this._algebraTimerWindow.open();
         this._algebraPromptWindow.show();
         this._algebraPromptWindow.open();
         this._algebraChoiceWindow.show();
@@ -278,12 +394,36 @@
         this._algebraChoiceWindow.activate();
     };
 
+    Scene_Battle.prototype.updateAlgebraQuestionTimer = function() {
+        if (!this._algebraChoiceWindow || !this._algebraChoiceWindow.active) {
+            return;
+        }
+        if (this._algebraQuestionTimedOut) {
+            return;
+        }
+        this._algebraQuestionTimerFrames--;
+        const remainingSeconds = this._algebraQuestionTimerFrames / 60;
+        this._algebraTimerWindow.setRemainingSeconds(remainingSeconds);
+        if (this._algebraQuestionTimerFrames <= 0) {
+            this.onAlgebraQuestionTimeout();
+        }
+    };
+
     Scene_Battle.prototype.closeAlgebraQuestion = function() {
         this._algebraChoiceWindow.deactivate();
+        this._algebraTimerWindow.close();
         this._algebraPromptWindow.close();
         this._algebraChoiceWindow.close();
+        this._algebraTimerWindow.hide();
         this._algebraPromptWindow.hide();
         this._algebraChoiceWindow.hide();
+    };
+
+    Scene_Battle.prototype.onAlgebraQuestionTimeout = function() {
+        this._algebraQuestionTimedOut = true;
+        SoundManager.playBuzzer();
+        this.closeAlgebraQuestion();
+        this.consumeMissedAttack("ran out of time!");
     };
 
     Scene_Battle.prototype.onAlgebraAnswerOk = function() {
@@ -301,20 +441,13 @@
         }
     };
 
-    Scene_Battle.prototype.onAlgebraAnswerCancel = function() {
-        this.closeAlgebraQuestion();
-        this._statusWindow.show();
-        this._actorCommandWindow.show();
-        this._actorCommandWindow.activate();
-    };
-
-    Scene_Battle.prototype.consumeMissedAttack = function() {
+    Scene_Battle.prototype.consumeMissedAttack = function(message) {
         const actor = BattleManager.actor();
         const action = BattleManager.inputtingAction();
         if (actor && action) {
             action.setSkill(AlgebraBattleQuestions.ensureSkipSkill());
             action._algebraMissedAttack = true;
-            this._logWindow.push("addText", `${actor.name()} loses the chance to attack!`);
+            this._logWindow.push("addText", `${actor.name()} ${message || "loses the chance to attack!"}`);
         }
         this.selectNextCommand();
     };
