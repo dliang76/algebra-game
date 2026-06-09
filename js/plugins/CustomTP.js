c5d2c56f478615d2a963eb8c5bf49ca964e0d283
Dennis Liang <dliang1122@gmail.com>
2026-06-08 20:51:15 -0400

    switched to time progression wait battle; let characters who die in battle revive with 1 HP

---
+++ js/plugins/CustomTP.js
@@ -0,0 +1,7 @@
+//=============================================================================
+// CustomStartingTP.js
+//=============================================================================
+
+Game_Battler.prototype.initTp = function() {
+    this.setTp(30); // Change the 0 to any starting number or formula you want
+};
